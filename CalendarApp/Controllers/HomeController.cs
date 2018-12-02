4c3b33060a5e0fadbb18f8b00e4347a6d074c627
Anu-PC\Anu <karpat62@gmail.com>
2018-12-02 16:57:13 +0200

    minor change

--- CalendarApp/Controllers/HomeController.cs
+++ CalendarApp/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace CalendarApp.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Calendar Application";
+            ViewBag.Title = "Calendar application";
 
             return View();
         }
